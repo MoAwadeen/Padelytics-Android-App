ff9786941a6a0649b5d1a1ffabc8bea67bd50e05
Merna Hesham <mernahesham2003@gmail.com>
2025-04-25 05:25:26 +0300

    About us screen

--- app/src/main/java/grad/project/padelytics/features/about/components/aboutComponents.kt
+++ app/src/main/java/grad/project/padelytics/features/about/components/aboutComponents.kt
@@ -1,7 +1,13 @@
 package grad.project.padelytics.features.about.components
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,17 +17,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -29,15 +39,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Scale
+import grad.project.padelytics.R
 import grad.project.padelytics.features.about.data.InfoPage
+import grad.project.padelytics.features.about.data.TeamInfo
+import grad.project.padelytics.features.auth.components.SmallGreenButton
 import grad.project.padelytics.ui.theme.Blue
 import grad.project.padelytics.ui.theme.BlueDark
+import grad.project.padelytics.ui.theme.GreenDark
 import grad.project.padelytics.ui.theme.GreenLight
 import grad.project.padelytics.ui.theme.lexendFontFamily
 import kotlinx.coroutines.launch
@@ -170,19 +190,199 @@ fun InfoScroller(pages: List<InfoPage>, onFinished: () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SmallGreenButton(label: String, onClick: () -> Unit ){
-    Button(onClick = { onClick() },
-        colors = ButtonDefaults.buttonColors(containerColor = GreenLight),
-        modifier = Modifier.width(160.dp).height(60.dp),
-        shape = RoundedCornerShape(100.dp),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+fun AboutUsAppToolbar() {
+    TopAppBar(
+        modifier = Modifier.fillMaxWidth().height(80.dp),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Blue,
+            titleContentColor = White,
+        ),
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(top = 6.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.padelytics_2),
+                    contentDescription = "Logo",
+                    modifier = Modifier.width(160.dp).height(36.dp)
+                )
+            }
+        }
     )
-    {
-        Text(label,
-            fontSize = 20.sp,
-            color = BlueDark ,
+}
+
+@Composable
+fun Titles(title: String){
+    Row(modifier = Modifier.fillMaxWidth()){
+        Text(
+            text = title,
+            style = TextStyle(
+                fontSize = 22.sp,
+                fontFamily = lexendFontFamily,
+                fontWeight = FontWeight.Bold,
+                color = BlueDark
+            )
+        )
+        Text(
+            text = ".",
+            style = TextStyle(
+                fontSize = 22.sp,
+                fontFamily = lexendFontFamily,
+                fontWeight = FontWeight.Bold,
+                color = GreenLight
+            )
+        )
+    }
+}
+
+@Composable
+fun Description(description: String){
+    Text(
+        text = description,
+        style = TextStyle(
+            fontSize = 16.sp,
             fontFamily = lexendFontFamily,
-            fontWeight = FontWeight.SemiBold)
+            fontWeight = FontWeight.Medium,
+            color = Blue
+        )
+    )
+}
+
+@Composable
+fun TitlesHeaders(title: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        HorizontalDivider(
+            modifier = Modifier.weight(1f),
+            thickness = 2.dp,
+            color = GreenLight
+        )
+
+        Text(
+            text = title,
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontFamily = lexendFontFamily,
+                fontWeight = FontWeight.Normal,
+                color = GreenDark
+            ),
+            modifier = Modifier.padding(horizontal = 8.dp),
+            textAlign = TextAlign.Center
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.weight(1f),
+            thickness = 2.dp,
+            color = GreenLight
+        )
+    }
+}
+
+@Composable
+fun MemberCard(member: TeamInfo, context: Context) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                try {
+                    val intent = Intent(Intent.ACTION_VIEW, member.memberLinkedIn.toUri())
+                    context.startActivity(intent)
+                } catch (e: Exception) {
+                    Log.e("AboutTeam", "Error opening URL: ${member.memberLinkedIn}", e)
+                    Toast.makeText(context, "Failed to open link", Toast.LENGTH_SHORT).show()
+                }
+            },
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape)
+                .background(White)
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(context)
+                    .data(member.memberPhoto)
+                    .crossfade(true)
+                    .scale(Scale.FILL)
+                    .build(),
+                contentDescription = "Member Image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(80.dp)
+                    .border(4.dp, Blue, CircleShape)
+                    .clip(CircleShape)
+                    .background(White)
+            )
+        }
+
+        Text(
+            text = member.memberName,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp),
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontFamily = lexendFontFamily,
+                fontWeight = FontWeight.SemiBold,
+                color = Blue
+            ),
+            textAlign = TextAlign.Center,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+
+        Text(
+            text = member.memberTitle,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontFamily = lexendFontFamily,
+                fontWeight = FontWeight.Normal,
+                color = GreenDark
+            ),
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun MemberInfoGrid(teamInfo: List<TeamInfo>) {
+    val context = LocalContext.current
+
+    if (teamInfo.isEmpty()) return
+
+    val isGrid = teamInfo.size > 1
+
+    if (isGrid) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(count = 2),
+            horizontalArrangement = Arrangement.spacedBy(30.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(max = 500.dp)
+                .padding(horizontal = 8.dp),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            items(teamInfo.size) { index ->
+                MemberCard(teamInfo[index], context)
+            }
+        }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.Start
+        ) {
+            MemberCard(teamInfo.first(), context)
+        }
     }
 }
--- app/src/main/java/grad/project/padelytics/features/about/ui/AboutUsScreen.kt
+++ app/src/main/java/grad/project/padelytics/features/about/ui/AboutUsScreen.kt
@@ -2,12 +2,13 @@ package grad.project.padelytics.features.about.ui
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,10 +16,97 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import grad.project.padelytics.R
+import grad.project.padelytics.features.about.components.AboutUsAppToolbar
+import grad.project.padelytics.features.about.components.Description
+import grad.project.padelytics.features.about.components.MemberInfoGrid
+import grad.project.padelytics.features.about.components.Titles
+import grad.project.padelytics.features.about.components.TitlesHeaders
+import grad.project.padelytics.features.about.data.TeamInfo
 import grad.project.padelytics.features.about.viewModel.AboutViewModel
 
 @Composable
 fun AboutUsScreen(modifier: Modifier = Modifier, navController: NavHostController, viewModel: AboutViewModel = viewModel()) {
+    val mentor = listOf(
+        TeamInfo(
+            memberName = "Dr. Samar Elbedwehy",
+            memberTitle = "Lecturer at KSU",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        )
+    )
+
+    val mobileAppTeam = listOf(
+        TeamInfo(
+            memberName = "Merna Hesham",
+            memberTitle = "Team Leader\nAndroid developer",
+            memberPhoto = R.drawable.merna,
+            memberLinkedIn = ""
+        ),
+        TeamInfo(
+            memberName = "Mohamed Awadeen",
+            memberTitle = "Android developer\nUi/Ux designer",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        ),
+        TeamInfo(
+            memberName = "Youssef Talaat",
+            memberTitle = "iOS developer",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        )
+    )
+
+    val aiTeam = listOf(
+        TeamInfo(
+            memberName = "Mostafa Gamal",
+            memberTitle = "Ai Engineer",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        ),
+        TeamInfo(
+            memberName = "Omar Gomaa",
+            memberTitle = "Ai Engineer",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        ),
+        TeamInfo(
+            memberName = "Dalia Ghazi",
+            memberTitle = "Ai Engineer",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        )
+    )
+
+    val webDevelopmentTeam = listOf(
+        TeamInfo(
+            memberName = "Mona Ahmed",
+            memberTitle = "Front end developer",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        ),
+        TeamInfo(
+            memberName = "Aya Mostafa",
+            memberTitle = "Back end developer",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        )
+    )
+
+    val documentationTeam = listOf(
+        TeamInfo(
+            memberName = "Rahma Emad",
+            memberTitle = "",
+            memberPhoto = R.drawable.rahma,
+            memberLinkedIn = ""
+        ),
+        TeamInfo(
+            memberName = "Rewan El-Hady",
+            memberTitle = "",
+            memberPhoto = null,
+            memberLinkedIn = ""
+        )
+    )
 
     BackHandler {
         navController.popBackStack()
@@ -26,16 +114,88 @@ fun AboutUsScreen(modifier: Modifier = Modifier, navController: NavHostControlle
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        topBar = {
+            AboutUsAppToolbar()
+        }
     ) { innerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = Color.White)
                 .padding(innerPadding)
-                .padding(start = 30.dp, end = 30.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .padding(start = 20.dp, end = 20.dp)
         ){
+            item{
+                Spacer(modifier = Modifier.height(16.dp))
 
+                Titles(title = "About us")
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Description(description = "We are a team of Artificial Intelligence students developing Padelytics as our graduation project, a mobile and web platform for padel performance analysis. This project reflects our passion for sports technology, innovation, and real-world AI applications.")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Titles(title = "Mission")
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Description(description = "Our mission is to empower padel players and coaches with intelligent tools that analyze performance through video and image processing. By using AI, computer vision and machine learning, we aim to deliver real-time, data-driven insights that support training, strategy, and player development.")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Titles(title = "Vision")
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Description(description = "Our vision is to become a leading platform in sports technology, making performance analysis accessible, smart, and engaging for the global padel community. We strive to push the boundaries of AI in sports and inspire innovation in athletic improvement.")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Titles(title = "Meet our team")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TitlesHeaders(title = "Our Mentor")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MemberInfoGrid(teamInfo = mentor)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TitlesHeaders(title = "Mobile App Team")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MemberInfoGrid(teamInfo = mobileAppTeam)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TitlesHeaders(title = "AI Team")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MemberInfoGrid(teamInfo = aiTeam)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TitlesHeaders(title = "Web Development Team")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MemberInfoGrid(teamInfo = webDevelopmentTeam)
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TitlesHeaders(title = "Documentation Team")
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MemberInfoGrid(teamInfo = documentationTeam)
+
+                Spacer(modifier = Modifier.height(16.dp))
+            }
         }
     }
 }
