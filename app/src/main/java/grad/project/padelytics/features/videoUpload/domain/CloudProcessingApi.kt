e8f2f77f40a1fee5cca75e3162f82db1118b53e6
Mohamed Awadeen <93440998+MoAwadeen@users.noreply.github.com>
2025-06-14 00:36:47 +0300

    Api handling

--- app/src/main/java/grad/project/padelytics/features/videoUpload/domain/CloudProcessingApi.kt
+++ app/src/main/java/grad/project/padelytics/features/videoUpload/domain/CloudProcessingApi.kt
@@ -1,14 +1,23 @@
 package grad.project.padelytics.features.videoUpload.domain
 
+import grad.project.padelytics.features.videoUpload.data.ResultResponse
 import grad.project.padelytics.features.videoUpload.data.VideoProcessingRequest
 import grad.project.padelytics.features.videoUpload.data.VideoProcessingResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+
 
 interface CloudProcessingApi {
     @POST("input-video/")
     suspend fun processVideo(
         @Body request: VideoProcessingRequest
     ): Response<VideoProcessingResponse>
+
+    @GET("result/{id}")
+    suspend fun getResult(
+        @Path("id") id: String
+    ): Response<ResultResponse>
 }
